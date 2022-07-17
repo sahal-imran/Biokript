1a6eca94cc5b48b30a0ed3d1b362cefa4f1c9c23
Sahal Imran <sahalimran786@gmail.com>
2022-07-17 12:55:27 +0500

    videp error solving

--- components/VideoIntro/VideoIntro.jsx
+++ components/VideoIntro/VideoIntro.jsx
@@ -8,7 +8,7 @@ function VideoIntro() {
     const router = useRouter();
     useEffect(() => {
       setTimeout(() => {
-        router.push('/home');
+        // router.push('/home');
       }, 7000);
     
       return () => {
@@ -21,14 +21,14 @@ function VideoIntro() {
         <Box className='player-wrapper' sx={{ width: '100%', height: '100vh', overflow: 'hidden', position: 'relative', background: 'black' }} >
             <ReactPlayer
                 className='react-player'
-                url='/video/video2.mp4'
+                url={'/video/video2.mp4'}
                 width='100%'
                 height='100%'
                 // volume={0.1}
-                controls={false}
-                muted={false}
-                playing={true}
-                loop={false}
+                controls={true}
+                // muted={false}
+                playing={1}
+                // loop={false}
             />
         </Box>
     )
