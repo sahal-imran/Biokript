85d14ec436ed9be5f68e39179728f6b6ce6fbb39
Sahal Imran <sahalimran786@gmail.com>
2022-07-18 15:40:14 +0500

    video playing error set

--- components/VideoIntro/VideoIntro.jsx
+++ components/VideoIntro/VideoIntro.jsx
@@ -1,37 +1,30 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import Box from "@mui/material/Box";
 import ReactPlayer from 'react-player';
 import { useRouter } from 'next/router';
 
 function VideoIntro() {
 
-    const router = useRouter();
-    useEffect(() => {
-      setTimeout(() => {
-        router.push('/home');
-      }, 8000);
-    
-      return () => {
-        // setTimeout()
-      }
-    }, [])
-    
+  const router = useRouter();
 
-    return (
-        <Box className='player-wrapper' sx={{ width: '100%', height: '100vh', overflow: 'hidden', position: 'relative', background: 'black' }} >
-            <ReactPlayer
-                className='react-player'
-                url={'/video/video.mp4'}
-                width='100%'
-                height='100%'
-                volume={0.1}
-                muted={true}
-                playing={true}
-                // controls={true}
-                // loop={false}
-            />
-        </Box>
-    )
+
+
+  return (
+    <Box className='player-wrapper' sx={{ width: '100%', height: '100vh', overflow: 'hidden', position: 'relative', background: 'black' }} >
+      <ReactPlayer
+        className='react-player'
+        url={'/video/video.mp4'}
+        width='100%'
+        height='100%'
+        volume={0.1}
+        muted={true}
+        playing={true}
+        onEnded={() => router.push('/home')}
+      // controls={true}
+      // loop={false}
+      />
+    </Box>
+  )
 }
 
 export default VideoIntro
