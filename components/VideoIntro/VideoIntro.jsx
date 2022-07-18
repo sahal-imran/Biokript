e269d74e07c290551326da4198c644d920d6108c
Sahal Imran <sahalimran786@gmail.com>
2022-07-18 19:35:17 +0500

    fixing phase 1

--- components/VideoIntro/VideoIntro.jsx
+++ components/VideoIntro/VideoIntro.jsx
@@ -6,11 +6,18 @@ import { useRouter } from 'next/router';
 function VideoIntro() {
 
   const router = useRouter();
+  useEffect(() => {
+  setTimeout(() => {
+    router.push('/home')
+  }, 8000);
+    
+  }, [])
+  
 
 
 
   return (
-    <Box className='player-wrapper' sx={{ width: '100%', height: '100vh', overflow: 'hidden', position: 'relative', background: 'black' }} >
+    <Box className='player-wrapper' sx={{ width: '100%', height: '100vh', overflow: 'hidden', position: 'relative', background: 'black' }}>
       <ReactPlayer
         className='react-player'
         url={'/video/video.mp4'}
@@ -19,7 +26,7 @@ function VideoIntro() {
         volume={0.1}
         muted={true}
         playing={true}
-        onEnded={() => router.push('/home')}
+        // onEnded={() => router.push('/home')}
       // controls={true}
       // loop={false}
       />
