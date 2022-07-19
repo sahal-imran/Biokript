f9f8daa6f69ada66028d874ec34fc80e6f1ece0d
Sahal Imran <sahalimran786@gmail.com>
2022-07-19 10:57:09 +0500

    navbar bg set at home page

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -35,9 +35,9 @@ function Hero() {
 
     return (
         <>
-            <Box sx={{ position: 'relative', overflow: 'hidden' }} >
+            <Box sx={{ position: 'relative', overflow: 'hidden',mt:'-70px' }} >
                 <Box>
-                    <Slider ref={c => (slider.current = c)} style={{ width: '100vw', height: 'calc(100vh - 70px)' }} {...settings}>
+                    <Slider ref={c => (slider.current = c)} style={{ width: '100vw', height: 'calc(100vh)' }} {...settings}>
                         <Box sx={{ width: '100%', height: '100%', position: 'relative' }} >
                             <Box sx={{ width: '100%', height: "100%" }} >
                                 <img style={{ width: '100%', height: '100vh', objectFit: 'cover' }} src="/slide1.jpg" alt="slideBG" />
